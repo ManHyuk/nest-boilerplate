2871baefeaa7f3f4ce9c91163b2a8d07b8e406a2
ManHyuk <rnanhyuk@gmail.com>
2020-11-15 00:19:49 +0900

    refact: 파일구조 변경

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/local-auth.guard';
+import { LocalAuthGuard } from './guard/local-auth.guard';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { JwtAuthGuard } from './guard/jwt-auth.guard';
 
 @Controller()
 export class AppController {
